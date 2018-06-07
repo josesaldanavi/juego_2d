978980ca8cbb6b7b8823d4ced3ff9b76a4032d81
Jose Saldaña <josesaldanavi@gmail.com>
2018-06-07 14:58:35 -0500

    area51

---
+++ juego_plataforma/Assets/Plataforma/Scripts/Check.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : MonoBehaviour {
+    //checkear la collision contra el suelo
+    // Use this for initialization
+    private Player_Controller player;
+
+    void Start()
+    {
+        player = GetComponentInParent<Player_Controller>();
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        player.grounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        player.grounded = false;
+    }
+}
---
+++ juego_plataforma/Assets/Plataforma/Scripts/Player_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+    public float velocidad=2f;
+    private Rigidbody2D rbd2;
+    public bool grounded;
+    private Animator anim;
+    private float maxForce=5;
+
+	// Use this for initialization
+	void Start () {
+        rbd2=GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("Speed", Mathf.Abs(rbd2.velocity.x));
+        anim.SetBool("Grounded", grounded);
+	}
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rbd2.AddForce(Vector2.right * h * velocidad);
+
+        float limitForce = Mathf.Clamp(rbd2.velocity.x,-maxForce,maxForce);
+            rbd2.velocity = new Vector2(limitForce, rbd2.velocity.y);
+        
+    }
+}
