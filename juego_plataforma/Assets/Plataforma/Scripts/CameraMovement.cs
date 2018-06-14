e6da40e4e0d3242dd67d057b5145d86456c85679
Jose Saldaña <josesaldanavi@gmail.com>
2018-06-14 17:17:45 -0500

    camera, lvl3

---
+++ juego_plataforma/Assets/Plataforma/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform target;
+    public Vector3 targetPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, target.position + targetPoint, 5f * Time.deltaTime);
+	}
+}
