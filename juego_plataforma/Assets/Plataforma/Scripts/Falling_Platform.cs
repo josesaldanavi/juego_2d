bb7ddce722ed46d5ba5f3b3c36783c270b72a215
josesaldanavi <josesaldanavi@gmail.com>
2018-06-14 19:50:50 -0500

    Falling platform

--- juego_plataforma/Assets/Plataforma/Scripts/Falling_Platform.cs
+++ juego_plataforma/Assets/Plataforma/Scripts/Falling_Platform.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Falling_Platform : MonoBehaviour {
     private Rigidbody2D rbd2d;
     private PolygonCollider2D pol;
+    public float time=1f;
+    public float respawnDelay = 1f;
+    Vector3 start;
 
 	// Use this for initialization
 	void Start () {
         rbd2d = GetComponent<Rigidbody2D>();
         pol = GetComponent<PolygonCollider2D>();
+        start = transform.position;
+
 	}
 	
 	// Update is called once per frame
@@ -17,13 +22,26 @@ public class Falling_Platform : MonoBehaviour {
 		
 	}
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
         if (other.gameObject.CompareTag("Player"))
         {
-            //hace que sea dinamico automaticamente
-            rbd2d.isKinematic = false;
-            pol.isTrigger = true;
+            //Invoca un mentodo y lo hace despues de un cierto tiempo
+            Invoke("Fall",time);
+            Invoke("Respawn", time+respawnDelay);
         }
+	}
+
+    void Fall(){
+        //hace que sea dinamico automaticamente
+        rbd2d.isKinematic = false;
+        pol.isTrigger = true;
+    }
+    void Respawn(){
+        transform.position = start;
+        rbd2d.isKinematic = true;
+        pol.isTrigger = false;
+        rbd2d.velocity = Vector3.zero;
     }
 }
